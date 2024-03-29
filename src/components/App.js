8007cc463a2d8872dcf1e5fe1b95862fa1c5e214
Hastle <alexandrs.me.com@gmail.com>
2024-03-29 16:29:42 +0300

    reindent, base example using grid

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,27 @@
-import React, { Component } from 'react';
+import React from "react";
+import "../styles/grid.sass";
 
-class App extends Component {
-
-    render() {
-        return (
-            <div>
-                <h1>Hello world!</h1>
-            </div>
-        );
-    }
-
+function App() {
+	return (
+		<>
+			<div className="container">
+				<div className="row g-3">
+					<div className="col-md-4">
+						<div id="card">Test 0</div>
+					</div>
+					<div className="col-md-4">
+						<div id="card">Text 1 $$</div>
+					</div>
+					<div className="col-md-4">
+						<div id="card">Test</div>
+					</div>
+					<div className="col-md-12">
+						<div id="card">Text #3</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default App;
